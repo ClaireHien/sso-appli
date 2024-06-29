10ec0ecddb4b2ab643cc9cbc583a46966902af04
ClaireH <clairehien.osmont@gmail.com>
2024-06-29 18:57:14 +0200

    prodBDD

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@ import { environment as local } from './environment.development';
 export const environment = {
   ...local,
   // Autres configurations spécifiques au développement ici
-  production: false,
-  backendUrl: 'http://sso-api.test/api'
+  //production: false,
+  //backendUrl: 'http://sso-api.test/api'
 
-  //production: true,
-  //backendUrl: 'https://sso-api.nhei.fr/api'
+  production: true,
+  backendUrl: 'https://sso-api.nhei.fr/api'
 };
